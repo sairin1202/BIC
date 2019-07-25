1601d5dca34f1d42a60b35921616aa9f38cc3427
sairin1202 <952141617@qq.com>
2019-07-25 16:14:42 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -15,14 +15,11 @@ class BatchData(Dataset):
         self.input_transform = input_transform
 
     def __getitem__(self, index):
-        # print("1",self.images[index].shape)
         image = self.images[index]
         image = Image.fromarray(np.uint8(image))
-        # print("3",image.size)
         label = self.labels[index]
         if self.input_transform is not None:
             image = self.input_transform(image)
-        # print("4",image.size())
         label = torch.LongTensor([label])
         return image, label
 
